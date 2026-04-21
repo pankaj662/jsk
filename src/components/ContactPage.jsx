aac6a04d9f41e7c373d2b71fa0b998415f174888
Pankaj <pankajdhakad585@gmail.com>
2026-04-21 12:33:14 +0530

    Time : 12:33

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -138,7 +138,7 @@ export default function ContactPage({ isOpen, onClose }) {
               <h3 className={styles.mapTitle}>📍 Our Location</h3>
               <div className={styles.mapFrame}>
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3559.5476!2d75.78!3d26.82!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjbCsDQ5JzEyLjAiTiA3NcKwNDYnNDguMCJF!5e0!3m2!1sen!2sin!4v1600000000000"
+                  src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3653!2d72.770988!3d23.303996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDE4JzE0LjQiTiA3MsKwNDYnMTUuNiJF!5e0!3m2!1sen!2sin!4v1713700000000!5m2!1sen!2sin"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
